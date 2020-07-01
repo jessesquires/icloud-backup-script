4630d0395f5445dcff9bb5d653c412a2faf1d8aa
Jesse Squires <jesse@jessesquires.com>
2020-06-30 20:34:11 -0700

    emoji

--- icloud_backup.sh
+++ icloud_backup.sh
@@ -5,14 +5,14 @@ DEST="/Volumes/Data1/Documents/icloud-backup/"
 USER="$(whoami)"
 SRC="/Users/$USER/Library/Mobile Documents/"
 
-echo "Source: $SRC"
-echo "Destination: $DEST"
-echo "Starting rsync backup..."
+echo "⏩  Source: $SRC"
+echo "⏩  Destination: $DEST"
+echo "⏩  Starting rsync backup..."
 echo ""
 
 # for testing: use --dry-run
 rsync --verbose --recursive --prune-empty-dirs --delete-before --whole-file --times --exclude=".DS_Store" --exclude=".Trash/" "$SRC" "$DEST"
 
 echo ""
-echo "Done!"
+echo "🌈  Done!"
 echo ""
